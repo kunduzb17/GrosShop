87dd22dd3c5afa555b973753915016845f2522fb
kunduzb17 <baryktabasovakunduz17@gmail.com>
2018-12-21 16:57:42 +0900

    Initial commit

---
+++ app/src/main/java/com/example/user/gros/Database.java
@@ -0,0 +1,4 @@
+package com.example.user.gros;
+
+public class Database {
+}
---
+++ app/src/main/java/com/example/user/gros/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.user.gros;
+
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
